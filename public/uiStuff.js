1231f61ac2cca95efa96110c2c8b7aebd8aacd3f
Martin <marb61a@gmail.com>
2020-07-22 13:19:22 +0100

    Updated uistuff js file

--- public/uiStuff.js
+++ public/uiStuff.js
@@ -1,8 +1,33 @@
 let wHeight = $(window).height();
 let wWidth = $(window).width();
 
+// Will handle all name values entered
+let player = {};
+
 let canvas = document.querySelector('#the-canvas');
 // Can be set to either 2d or Webgl\2
 let context = canvas.getContext('2d');
 canvas.width = wWidth;
 canvas.height = wHeight;
+
+$(window).load(() => {
+    $('#loginModal').modal('show')
+});
+
+// For adding a name while playing as a guest
+$('.name-form').submit((event) => {
+    event.preventDefault();
+    // console.log('Submitted');
+
+    player.name = document.querySelector('#name-input').value;
+    $('#loginModal').modal('hide');
+    $('#spawnModal').modal('show');
+
+    // Grabs the player name for the greeting
+    document.querySelector('.player-name').innerHTML = player.name;
+});
+
+// Event is not needed here as there is not default behaviour
+$('.start-game').click((event) => {
+
+});
