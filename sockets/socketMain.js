a30782959eca572cc1cc7f05442f1dd16590f9e9
Martin <marb61a@gmail.com>
2020-07-23 19:35:45 +0100

    Updated  Orbs.js and added functions to socketMain js file

--- sockets/socketMain.js
+++ sockets/socketMain.js
@@ -1,4 +1,21 @@
 // Where socketio stuff goes
 const io = require('../servers').io;
+const Orb = require('./classes/Orb');
+let orbs =[];
+
+initGame();
+
+io.sockets.on('connect', (socket) => {
+    socket.emit('init', {
+        orbs
+    });
+});
+
+// Ran at the beginning of a new game
+function init(){
+    for(let i = 0; i < 500; i++){
+        orbs.push(new Orb());
+    }
+}
 
 module.exports = io;
